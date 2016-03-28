9046d476eed1c1705fe68a21edc16a64f8eec99a
amadhav1 <amadhav1@jhu.edu>
2016-03-28 14:42:36 -0400

    copied the index file

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <section class="main">
       <h1><?php the_title(); ?></h1>
+	   <?php if (has_post_thumbnail()) : ?>
+        <div style="text-align:center;">
+          <?php the_post_thumbnail('full'); ?>
+        </div>
+      <?php endif; ?>
       <?php the_content(); ?>
     </section>
   <?php endwhile; endif; ?>
