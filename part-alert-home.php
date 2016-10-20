f1a0aad63e32c4bf20209bfd1b18e39e3635054e
amadhav1 <amadhav1@jhu.edu>
2016-10-20 16:28:39 -0400

    fixed the alert code so it stoped giving errors

--- part-alert-home.php
+++ part-alert-home.php
@@ -5,7 +5,8 @@ if ( is_page_template( 'template-home.php' ) ) {
 $args = array( 
  
 'category__and'=>array( 27 ),
-'category__not_in' => array( 26 ),
+'category_name' => 'site-emergency-alerts', 
+'category__not_in' => array( 4 ),
   'orderby'    =>  date,
   'order'      =>  ASC,
  'posts_per_page' => 1
@@ -16,9 +17,10 @@ $args = array(
 $myposts = get_posts( $args );
 foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 	<div class="rave-alert site">
-	<?php echo '<a href="'.get_permalink().'" >'.get_the_title().'</a>';
+	<?php echo '<h3><a href="'.get_permalink().'" >'.get_the_title().'</a></h3>';
 			?>
-	</div>
+		</div>
+	
 <?php endforeach; 
 wp_reset_postdata();
 } else {?>
@@ -26,7 +28,7 @@ wp_reset_postdata();
 include_once( ABSPATH . WPINC . '/feed.php' );
 
 // Get a SimplePie feed object from the specified feed source.
-$rss = fetch_feed( 'http://studentaffairs.jhu.edu/feed/?cat=27' );
+$rss = fetch_feed(array('http://studentaffairs.jhu.edu/feed/?cat=27','http://www.getrave.com/rss/jhu/channel2', 'http://www.getrave.com/rss/jhu/channel4', 'http://www.getrave.com/rss/jhu/channel5'));
 
 if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
@@ -40,43 +42,59 @@ endif;
 ?>
 
 
-    <?php if ( $maxitems == 0 ) : ?>
-       
-    <?php else : ?>
-        <?php // Loop through each feed item and display each item as a hyperlink. ?>
-        <?php foreach ( $rss_items as $item ) : ?>
-          <div class="rave-alert site">
+<?php if ( $maxitems == 0 ) : ?>
+   
+<?php else : ?>
+	<?php // Loop through each feed item and display each item as a hyperlink. ?>
+	<?php foreach ( $rss_items as $item ) : ?>
+	<?php if($item->get_content()=='NO ALERT'){echo '';}else{ 
+	
+	if ($item->get_feed()->get_title()=='JHU.edu website alert: RED'){
+	echo '<div class="rave-alert red">';
+	  echo '<a href="https://www.jhu.edu/alert">'.esc_html( $item->get_title()). '</a>';
+		echo '</div>';
+	}
+	elseif($item->get_feed()->get_title() == 'JHU.edu website alert: YELLOW'){
+	echo '<div class="rave-alert yellow">';
+	  echo '<a href="https://www.jhu.edu/alert">'.esc_html( $item->get_title()). '</a>';
+		echo '</div>';
+		}
+		elseif($item->get_feed()->get_title() == 'JHU.edu website alert: GREEN'){
+		echo '<div class="rave-alert green">';
+		  echo '<a href="https://www.jhu.edu/alert">'.esc_html( $item->get_title()). '</a>';
+		echo '</div>';
+		}
+		elseif($item->get_feed()->get_title() == 'Site Emergency Alerts – Homewood Student Affairs'){
+		echo '<div class="rave-alert site">';
+		  echo '<a href="'.esc_url( $item->get_permalink() ).'">'.esc_html( $item->get_title() ).'</a>';
+		echo '</div>';
+		}
+		
+		 /*<div class="rave-alert site">
                 <a href="<?php echo esc_url( $item->get_permalink() ); ?>">
                     <?php echo esc_html( $item->get_title() ); ?>
 				
                 </a>
-		</div>
-        <?php endforeach; ?>
-    <?php endif; 
-}
-?>
-
-<?php 
-$feed = fetch_feed(array('http://www.getrave.com/rss/jhu/channel2', 'http://www.getrave.com/rss/jhu/channel4', 'http://www.getrave.com/rss/jhu/channel5'));
-
-// Loop the results
-foreach($feed->get_items(0,1) as $item) {
-	if($item->get_content()=='NO ALERT'){echo '';}else{ 
-	
-	if ($item->get_feed()->get_title()=='JHU.edu website alert: RED'){
-	echo '<div class="rave-alert red">';
-	}
-	elseif($item->get_feed()->get_title() == 'JHU.edu website alert: YELLOW'){
-	echo '<div class="rave-alert yellow">';
-		}
-		elseif($item->get_feed()->get_title() == 'JHU.edu website alert: GREEN'){
-		echo '<div class="rave-alert green">';
-		}
+		</div>*/
+		
 		else{
 			echo '';
 		}
-    echo '<a href="https://www.jhu.edu/alert">'.$item->get_content(). '</a>';
-		echo '</div>';
+	
+	  
 
 	}
-}?>
+
+	
+	?>
+	
+	
+	
+	
+	
+		
+		
+	<?php endforeach; ?>
+<?php endif; 
+}
+?>
