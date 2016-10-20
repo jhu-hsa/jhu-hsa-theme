89f24eaabe207b1045e4593179cdd0e7b2b04d3e
amadhav1 <amadhav1@jhu.edu>
2016-10-20 15:48:42 -0400

    new template so we have a page without a sidebar

---
+++ template-nosb.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Special No Sidebar
+*/
+?>
+<?php get_header(); ?>
+
+<?php get_template_part( 'part', 'breadcrumbs' ); ?>
+
+<div class="wrap">
+
+
+
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <section class="nosb">
+      <h1><?php the_title(); ?></h1>
+   <?php if (has_post_thumbnail()) : ?>
+        <div style="text-align:center;">
+          <?php the_post_thumbnail('full'); ?>
+        </div>
+      <?php endif; ?>
+      <?php the_content(); ?>
+    </section>
+  <?php endwhile; endif; ?>
+
+  
+
+</div>
+
+
+
+<?php get_template_part( 'part', 'resource-finder' ); ?>
+
+<?php get_footer(); ?>
