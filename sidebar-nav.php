5f6ba903b8196e3abf8a052ca0546e67aa9cc604
amadhav1 <amadhav1@jhu.edu>
2017-07-19 13:42:08 -0400

    changed menu to be Main Navigation

--- sidebar-nav.php
+++ sidebar-nav.php
@@ -1,8 +1,9 @@
 <section class="sidebar" role="region" aria-label="main nav">
   <div class="sidebar__block">
     <a class="sidebar__nav__toggle">In this section...</a>
-    <ul class="sidebar__nav">
-      <?php wp_list_pages( array( 'title_li' => '' ) ); ?>
-    </ul>
+  <?php wp_nav_menu(array(
+	'menu' => 'Main Navigation',
+	'container' => 'ul',
+	'menu_class' => 'sidebar__nav'  ));	  ?>
   </div>
 </section>
