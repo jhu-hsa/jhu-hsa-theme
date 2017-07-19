c37908c17d642223fba41dbb2b908ba488ff5b81
amadhav1 <amadhav1@jhu.edu>
2017-07-19 13:40:00 -0400

    modified to use svg for logo

--- part-logo-title.php
+++ part-logo-title.php
@@ -1,5 +1,5 @@
 <header class="header"  role="banner">
-        <a class="header__logo" href="//www.jhu.edu"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Johns Hopkins University"></a>
+        <a class="header__logo" href="//www.jhu.edu"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Johns Hopkins University"></a>
         <div class="header__title">
           <?php switch_to_blog(1); $network_site_title = get_bloginfo('name'); restore_current_blog(); ?>
           <a class="header__title__main" href="<?php echo network_site_url(); ?>"><?php echo $network_site_title; ?></a>
