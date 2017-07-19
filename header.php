856516394cec478647eef5e015a62ab6fdf7b037
amadhav1 <amadhav1@jhu.edu>
2017-07-19 13:38:09 -0400

    added google analytics code

--- header.php
+++ header.php
@@ -21,7 +21,16 @@ var b=document.getElementsByTagName("script")[0];
 a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0044/9740.js?"+Math.floor(new Date().getTime()/3600000);
 a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
 </script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-64979302-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 <?php }?>
   
